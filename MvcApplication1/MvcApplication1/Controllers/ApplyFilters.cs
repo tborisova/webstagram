5e7a5bb2740694d5d2af8f5b19cf01dbe75bd5a0
Dimitar Pankov <mitelinko@gmail.com>
2012-12-16 12:41:07 +0200

    Added new ASP.NET MVC realization
    Filters working slowly.

---
+++ MvcApplication1/MvcApplication1/Controllers/ApplyFilters.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Web;
+using ImageProcessing;
+
+namespace CustomApplyFilterExtension
+{
+    public static class CustomApplyFilters
+    {
+        public static ImageProcessor imageProcessor = new ImageProcessor();
+
+        public static string ApplyFilters(this Bitmap img)
+        {
+            imageProcessor.SetImage(img);
+            string result = null;
+            
+            imageProcessor.ApplyInvert();
+            result += BitmapToString(imageProcessor.GetImage());
+
+            imageProcessor.ApplySepia(20);
+            result += BitmapToString(imageProcessor.GetImage());
+
+            return result;
+        }
+
+        private static string BitmapToString(Bitmap img)
+        {
+            string bitmapString = null;
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                img.Save(memoryStream, ImageFormat.Jpeg);
+                byte[] bitmapBytes = memoryStream.GetBuffer();
+                bitmapString = Convert.ToBase64String(bitmapBytes, Base64FormattingOptions.InsertLineBreaks);
+            }
+            bitmapString = "data:image/jpeg;base64," + bitmapString + " ";
+            return bitmapString;
+        }
+    }
+}
